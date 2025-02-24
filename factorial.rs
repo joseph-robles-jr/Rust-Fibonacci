f1b7d83bcca1315145764f5521a1a2489a5dcec8
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2025-02-24 13:32:00 -0700

    began a help dialogue

--- factorial.rs
+++ factorial.rs
@@ -5,10 +5,15 @@ use std::io::{self, Write};
 
 fn main() {
     let mut menu_choice = 0;
-    while  menu_choice != 3{
+    while  menu_choice != 8{
         println!("1. Find factorial");
         println!("2. Find fibonacci");
-        println!("3. Quit");
+        println!("3. ");
+        println!("4. ");
+        println!("5. ");
+        println!("6. ");
+        println!("7. Help");
+        println!("8. Quit ");
         print!("Enter your choice: ");
         let mut raw_input = String::new();
         io::stdout().flush().unwrap();
@@ -18,11 +23,41 @@ fn main() {
         if 1 == menu_choice{
             find_factorial();
         }
+        else if 2 == menu_choice{ }
+        else if 3 == menu_choice{ }
+        else if 7 == menu_choice{
+            print!("{}", info("factorial"));
+        }
         else{}
     }
  
 
 }
+
+fn info(opperation: &str) -> String{
+
+    let mut opperation_defs = Vec::new();
+
+    opperation_defs.push("A factorial of a non-negative integer n is the product of all positive integers less than or equal to n. It is denoted by n! and is defined as:
+    
+    n! = n * (n-1) * (n-2) * ... * 1
+    
+    For example:
+    5! = 5 * 4 * 3 * 2 * 1 = 120
+    3! = 3 * 2 * 1 = 6
+    0! = 1 (by definition)\n\n");
+
+    
+    let mut output = String::new();
+
+    if "factorial" == opperation {
+        
+        output = opperation_defs.get(0).expect("REASON").to_string();
+        
+    }
+    return output;
+}
+
 
 fn find_factorial() {
     print!("Enter a whole number: ");
