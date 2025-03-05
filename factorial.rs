42174eca21fcdf4ac185ce538543c405febceaa4
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2025-03-05 13:03:46 -0700

    added more modularity to how int input is collected. Created a fibinaci calulator .

--- factorial.rs
+++ factorial.rs
@@ -8,7 +8,7 @@ fn main() {
     while  menu_choice != 8{
         println!("1. Find factorial");
         println!("2. Find fibonacci");
-        println!("3. ");
+        println!("3. Find the average");
         println!("4. ");
         println!("5. ");
         println!("6. ");
@@ -23,8 +23,10 @@ fn main() {
         if 1 == menu_choice{
             find_factorial();
         }
-        else if 2 == menu_choice{ }
-        else if 3 == menu_choice{ }
+        else if 2 == menu_choice{ 
+            find_fibonacci();
+        }
+        else if 3 == menu_choice{  }
         else if 7 == menu_choice{
             print!("{}", info("factorial"));
         }
@@ -58,13 +60,24 @@ fn info(opperation: &str) -> String{
     return output;
 }
 
-
-fn find_factorial() {
+fn get_int() -> i64 {
     print!("Enter a whole number: ");
     io::stdout().flush().unwrap(); // flush the buffer to display the prompt
     let mut input = String::new(); //creates a new string
     io::stdin().read_line(&mut input).unwrap(); //reads the input and unwraps it
     let input:i64 = input.trim().parse().unwrap(); //parses the input and unwraps it
+    return input;
+
+}
+
+fn find_factorial() {
+    // print!("Enter a whole number: ");
+    // io::stdout().flush().unwrap(); // flush the buffer to display the prompt
+    // let mut input = String::new(); //creates a new string
+    // io::stdin().read_line(&mut input).unwrap(); //reads the input and unwraps it
+    // let input:i64 = input.trim().parse().unwrap(); //parses the input and unwraps it
+
+    let input:i64 = get_int();
     let output:i64; //creates a new string
     output = factorial(input);
     println!("The factorial of {} is {}", input, output);
@@ -79,3 +92,39 @@ fn factorial(n: i64) ->i64{
         return n * factorial(n - 1);
     }
 }
+
+fn find_fibonacci(){
+    let input:i64 = get_int();
+    let output:i64 = fibonacci(input);
+    println!("The fibinacci of {} is {}", input, output);
+
+}
+
+fn fibonacci(n: i64) -> i64 {
+    if n <= 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        const ARRAY_LENGTH:usize = 10; 
+        let times_to_loop:i64 = ARRAY_LENGTH as i64;  // Fixes an issue where the array expects a constant.
+       
+
+        let mut fib_arr: [i64; ARRAY_LENGTH] = [0; ARRAY_LENGTH];
+        let mut a = 0;
+        let mut b = 1;
+        let mut i:i64 = 0;
+        while i != times_to_loop - 1 {
+        for _ in 2..=n {
+            let temp = a + b;
+            a = b;
+            b = temp;
+            fib_arr[i as usize] = b;
+            i = i + 1;
+        }
+        }
+        return 404;
+    }
+}
+
+
